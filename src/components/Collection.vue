db58a85846d1f49792317ec0f6299f252d90a8d0
PyMonar <pymonar@foxmail.com>
2018-04-28 15:36:44 +0800

    change set structrue

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -8,64 +8,74 @@
         <div class="set-year">
           <span class="cn-year">渡鸦年</span>
           <span class="en-year">Raven</span>
-        </div>
-        <div class="set-expansion">
-          <span class="cn-expansion">女巫森林</span>
-          <span class="en-expansion">Delve Into The Witchwood</span>
-        </div>
-        <div class="set-main">
-          <el-tabs v-model="activeClass" @tab-click="changeClass">
-            <el-tab-pane name="zs">
-              <span slot="label" class="zs-color">
-                战士
-              </span>
-            </el-tab-pane>
-            <el-tab-pane name="lr">
-              <span slot="label" class="lr-color">
-                猎人
-              </span>
-            </el-tab-pane>
-            <el-tab-pane name="sm">
-              <span slot="label" class="sm-color">
-                萨满
-              </span>
-            </el-tab-pane>
-            <el-tab-pane name="dz">
-              <span slot="label" class="dz-color">
-                盗贼
-              </span>
-            </el-tab-pane>
-            <el-tab-pane name="fs">
-              <span slot="label" class="fs-color">
-                法师
-              </span>
-            </el-tab-pane>
-            <el-tab-pane name="xd">
-              <span slot="label" class="xd-color">
-                德鲁伊
-              </span>
-            </el-tab-pane>
-            <el-tab-pane name="qs">
-              <span slot="label" class="qs-color">
-                骑士
-              </span>
-            </el-tab-pane>
-            <el-tab-pane name="ms">
-              <span slot="label" class="ms-color">
-                牧师
-              </span>
-            </el-tab-pane>
-            <el-tab-pane name="ss">
-              <span slot="label" class="ss-color">
-                术士
-              </span>
-            </el-tab-pane>
-            <el-tab-pane name="zl">
-              <span slot="label" class="zl-color">
-                中立
-              </span>
-            </el-tab-pane>
-          </el-tabs>
+          <div class="set-expansion">
+            <span class="cn-expansion">女巫森林</span>
+            <span class="en-expansion">Delve Into The Witchwood</span>
+            <div class="set-main">
+              <el-tabs v-model="activeClass" @tab-click="changeClass">
+                <el-tab-pane name="zs">
+                  <span slot="label" class="zs-color">
+                    战士
+                  </span>
+                  0
+                </el-tab-pane>
+                <el-tab-pane name="lr">
+                  <span slot="label" class="lr-color">
+                    猎人
+                  </span>
+                  1
+                </el-tab-pane>
+                <el-tab-pane name="sm">
+                  <span slot="label" class="sm-color">
+                    萨满
+                  </span>
+                  2
+                </el-tab-pane>
+                <el-tab-pane name="dz">
+                  <span slot="label" class="dz-color">
+                    盗贼
+                  </span>
+                  3
+                </el-tab-pane>
+                <el-tab-pane name="fs">
+                  <span slot="label" class="fs-color">
+                    法师
+                  </span>
+                  4
+                </el-tab-pane>
+                <el-tab-pane name="xd">
+                  <span slot="label" class="xd-color">
+                    德鲁伊
+                  </span>
+                  5
+                </el-tab-pane>
+                <el-tab-pane name="qs">
+                  <span slot="label" class="qs-color">
+                    骑士
+                  </span>
+                  6
+                </el-tab-pane>
+                <el-tab-pane name="ms">
+                  <span slot="label" class="ms-color">
+                    牧师
+                  </span>
+                  7
+                </el-tab-pane>
+                <el-tab-pane name="ss">
+                  <span slot="label" class="ss-color">
+                    术士
+                  </span>
+                  8
+                </el-tab-pane>
+                <el-tab-pane name="zl">
+                  <span slot="label" class="zl-color">
+                    中立
+                  </span>
+                  9
+                </el-tab-pane>
+              </el-tabs>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -115,23 +125,24 @@ export default {
     .en-year {
       font-size: 15px;
     }
-  }
 
-  .set-expansion {
-    padding: 10px;
-    height: 25px;
-    line-height: 25px;
-    font-size: 18px;
-    color: mediumpurple;
+    .set-expansion {
+      margin-top: 10px;
+      padding: 10px;
+      height: 25px;
+      line-height: 25px;
+      font-size: 18px;
+      color: mediumpurple;
 
-    .en-expansion {
-      font-size: 10px;
+      .en-expansion {
+        font-size: 10px;
+      }
+
+      .set-main {
+        color: #b7bac0;
+        margin-top: 10px;
+      }
     }
-  }
-
-  .set-main {
-    color: #b7bac0;
-    padding-left: 10px;
   }
 }
 
